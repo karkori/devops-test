caf630ef2878bfd3702451da67f73336663c1ac7
Mostapha <femtonet.email@gmail.com>
2021-06-13 16:43:50 +0200

    fix: change root pass

--- addUser.sh
+++ addUser.sh
@@ -3,7 +3,9 @@
 # $2 : PASSWORD
 if [ "$(id -u $1)" != "1000" ]
 then
-    (useradd -m -u 1000 $1 && (echo "$1:$2" | chpasswd) && adduser $1 sudo)
-    echo "Usuario $1 creado con éxito"
+    useradd -m -u 1000 $1 && \
+    echo "$2\n$2\n" | passwd "$1" && \
+    usermod -aG sudo $1
+    echo "\nUsuario $1 creado con éxito"
 else echo "El usuario $1 ya existe."
 fi
